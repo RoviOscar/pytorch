5dc980f84107aea878ddc8a7b82aae35ae0e3add
Xiang Gao <qasdfgtyuiop@gmail.com>
2020-04-07 11:09:33 -0700

    Completely kill CUDAApplyUtils.cuh
    ghstack-source-id: 428ab9a27eeaaa35f1c6ad782fd3031c73a27ce4
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/36135

--- aten/src/ATen/native/cuda/Dropout.cu
+++ aten/src/ATen/native/cuda/Dropout.cu
@@ -1,7 +1,7 @@
 #include <ATen/ATen.h>
 #include <ATen/AccumulateType.h>
 #include <ATen/cuda/CUDAApplyUtils.cuh>
-#include <ATen/CUDAGeneratorImpl.h>
+#include <ATen/CUDAGenerator.h>
 #include <ATen/cuda/detail/IndexUtils.cuh>
 #include <ATen/cuda/detail/TensorInfo.cuh>
 #include <c10/macros/Macros.h>
